8ca8314a398bca728e16bcc8e22f843b3d7199f2
salhymed <salhy.mohamed@gmail.com>
2024-02-21 19:05:35 +0100

    adding left Navbar menu (sidebar)

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import './menu.scss'
+import { Link } from 'react-router-dom'
+import { menu } from '../../data.js';
 function Menu() {
   return (
-    <div className='menu'>Menu</div>
+    <div className='menu'>
+      {
+        menu.map(item => {
+          return (
+            <div className="item" key={item.id}>
+              <span className='title'>{item.title}</span>
+              {
+                item.listItem.map(listItem => {
+                  return (
+                    <Link key={listItem.id} to={listItem.url} className={listItem.active ? 'listItem active' : 'listItem'}>
+                      <img src={listItem.icon} alt="" />
+                      <span className="listItemTitle">{listItem.title}</span>
+                    </Link>
+                  )
+                })
+              }
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,125 @@
+export const menu = [
+    {
+        id: 1,
+        title: "Main",
+        listItem: [
+            {
+                id: 1,
+                title: "Home",
+                url: "/",
+                icon: "home.svg"
+            },
+            {
+                id: 2,
+                title: "Profile",
+                url: "/profile",
+                icon: "user.svg"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Lists",
+        listItem: [
+            {
+                id: 1,
+                title: "Users",
+                url: "/users",
+                icon: "user.svg"
+            },
+            {
+                id: 2,
+                title: "Products",
+                url: "/products",
+                icon: "product.svg",
+            },
+            {
+                id: 3,
+                title: "Orders",
+                url: "/orders",
+                icon: "order.svg"
+            },
+            {
+                id: 4,
+                title: "Posts",
+                url: "/posts",
+                icon: "post2.svg",
+                active: true
+            }
+
+        ]
+    },
+    {
+        id: 3,
+        title: "General",
+        listItem: [
+            {
+                id: 1,
+                title: "Elements",
+                url: "/elements",
+                icon: "element.svg"
+            },
+            {
+                id: 2,
+                title: "Notes",
+                url: "/notes",
+                icon: "note.svg"
+            },
+            {
+                id: 3,
+                title: "Orders",
+                url: "/orders",
+                icon: "order.svg"
+            },
+            {
+                id: 4,
+                title: "Forms",
+                url: "/forms",
+                icon: "form.svg"
+            },
+            {
+                id: 5,
+                title: "Calendars",
+                url: "/calendars",
+                icon: "calendar.svg"
+            }
+
+        ]
+    },
+    {
+        id: 4,
+        title: "Maintenance",
+        listItem: [
+            {
+                id: 1,
+                title: "Settings",
+                url: "/settings",
+                icon: "setting.svg"
+            },
+            {
+                id: 2,
+                title: "Backups",
+                url: "/backups",
+                icon: "backup.svg"
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Analytics",
+        listItem: [
+            {
+                id: 1,
+                title: "Charts",
+                url: "/charts",
+                icon: "chart.svg"
+            },
+            {
+                id: 2,
+                title: "Logs",
+                url: "/logs",
+                icon: "log.svg"
+            }
+        ]
+    }
+]
